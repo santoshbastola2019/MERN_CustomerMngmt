9dda124fe18ee373d78cf2c862cdc517a6726299
Santosh <santoshbanstola1111@gmail.com>
2024-06-20 17:12:54 +0545

    Post API and cors config

--- node-crud/app.js
+++ node-crud/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- react-crud/src/components/postUser/postUser.jsx
+++ react-crud/src/components/postUser/postUser.jsx
@@ -1,10 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import "./postUser.css";
+import { useNavigate } from "react-router-dom";
 
 const PostUser = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+  const navigate = useNavigate();
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(formData.name);
+    // console.log(formData.email);
+    // console.log(formData.phone);
+    try {
+      const response = await fetch("http://localhost:5000/api/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json(response);
+      console.log(data);
+      navigate("/");
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
-    <div>
-      <h1>PostUser Component</h1>
-    </div>
+    <>
+      <div className="center-form">
+        <h1>Post New User</h1>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              placeholder="Enter name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicName">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicName">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control
+              type="phone"
+              name="phone"
+              placeholder="Enter phone"
+              value={formData.phone}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <Button variant="dark" type="submit" className="w-100">
+            Post User
+          </Button>
+        </Form>
+      </div>
+    </>
   );
 };
 
