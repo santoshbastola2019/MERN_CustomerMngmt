1bb0f79c294dc253032e3ab7ef69a17e7462270b
Santosh <santoshbanstola1111@gmail.com>
2024-06-20 15:47:12 +0545

    Create Navbar

--- react-crud/src/components/header/header.jsx
+++ react-crud/src/components/header/header.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./header.css";
 
 const Header = () => {
   return (
     <div>
-      <h1>Header Component</h1>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            My App
+          </Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/" className="nav-link">
+              Dashboard
+            </Nav.Link>
+            <Nav.Link as={Link} to="/user" className="">
+              Post User
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 };
